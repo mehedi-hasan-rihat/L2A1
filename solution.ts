5fd53902dc023bd9e9042046f4ed24736a4bc08a
Mehedi Hasan Rihat <mehedi4794@gmail.com>
2025-11-17 10:26:30 +0600

    Add solution for Problem 6: Define Book interface and implement printBookDetails function

--- solution.ts
+++ solution.ts
@@ -71,3 +71,27 @@ type User = {
 function filterActiveUsers(users: User[]): User[] {
   return users.filter(user => user.isActive === true);
 }
+
+
+
+
+// interface Book {
+//   title: string;
+//   author: string;
+//   publishedYear: number;
+//   isAvailable: boolean;
+// }
+
+// function printBookDetails(book: Book): void {
+//   const availability = book.isAvailable ? "Yes" : "No";
+//   console.log(`Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`);
+// }
+
+// const myBook: Book = {
+//   title: 'The Great Gatsby',
+//   author: 'F. Scott Fitzgerald',
+//   publishedYear: 1925,
+//   isAvailable: true,
+// };
+
+// printBookDetails(myBook);
